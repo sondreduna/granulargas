ea58e26cfd6f5b7c3627216b15eeb977e8d51f5b
Sondre Duna Lundemo <sondre@duna.no>
2021-02-17 20:13:03 +0100

    Finished prosject 1 more or less
    Some proofreading left, but that is not curcial as the report is not to be handed in yet

--- src/problem_4.py
+++ src/problem_4.py
@@ -1,6 +1,9 @@
 from events import *
 from plotting import *
 
+from mpltools import annotation  # The special package mpltools may need to be installed
+                                 # To get fancy slope marker
+
 def distribute_balls(N,max_iter = 10000):
 
     """
@@ -48,14 +51,14 @@ def crater_sim(N,xi = 0.5, M = 10, path = "../fig/crater.pdf"):
     gas.M[1:] = m
     gas.xi = xi
 
-    gas.particles[:,0] = np.array([0.5,0.75,0,-5])
+    gas.particles[:,0] = np.array([0.5,0.75,0,-1])
     gas.particles[:2,1:] = X
     
-    #gas.simulate(dt = 1, stopper = "dissipation", stop_val = 0.9, ret_vels = False)
-    gas.simulate_savefigs(1, 0.005, False)
+    gas.simulate(dt = 1, stopper = "dissipation", stop_val = 0.9, ret_vels = False)
+    #gas.simulate_savefigs(1, 0.005, False)
     gas.plot_positions(path)
     
-def crater(N,xi = 0.5,M = 25):    
+def crater(N,xi = 0.5,M = 25, v = 5):    
     m = 1
     M = M
 
@@ -68,7 +71,7 @@ def crater(N,xi = 0.5,M = 25):
     gas.M[1:] = m
     gas.xi = xi
 
-    gas.particles[:,0] = np.array([0.5,0.75,0,-5])
+    gas.particles[:,0] = np.array([0.5,0.75,0,-v])
     gas.particles[:2,1:] = X
 
     gas.simulate(dt = 1, stopper = "dissipation", stop_val = 0.9, ret_vels = False)
@@ -77,18 +80,18 @@ def crater(N,xi = 0.5,M = 25):
     x = gas.particles[:2,:]
 
     # plot the distribution of particles, for debugging 
-    #gas.plot_positions("../fig/crater.pdf")
+    gas.plot_positions("../fig/crater.pdf")
 
     # number of particles involved in crater formation:  
-    affected = N -  np.count_nonzero( gas.count == 0 )
+    affected = np.count_nonzero(gas.count)
     return affected
 
 def double_crater_test():
     m1 = 5
     m2 = 25
 
-    crater_sim(2000,M = m1,path = "../fig/crater_1.pdf")
-    crater_sim(2000,M = m2,path = "../fig/crater_2.pdf")
+    crater_sim(4000,M = m1,path = "../fig/crater_1.pdf")
+    crater_sim(4000,M = m2,path = "../fig/crater_2.pdf")
 
 def mass_scan(num_masses = 10,label = 0,min_mass = 1, max_mass = 25):
     """
@@ -105,7 +108,17 @@ def mass_scan(num_masses = 10,label = 0,min_mass = 1, max_mass = 25):
     np.save(f"../data/prob4/M_{label}.npy",M)
     np.save(f"../data/prob4/size_{label}.npy",size)
 
+def vel_scan(num_vels = 10, label = 0):
 
+    V = np.logspace(-1,2,num_vels)
+    size = np.zeros(num_vels)
+
+    for i, v_i in enumerate(V):
+        size[i] = crater(2000,xi = 0.5, M = 20, v = v_i)
+
+    np.save(f"../data/prob4/v_{label}.npy",V)
+    np.save(f"../data/prob4/vs_{label}.npy",size)
+    
 def problem_4_plot():
 
     
@@ -124,13 +137,33 @@ def problem_4_plot():
 
     fig.savefig("../fig/mass_size.pdf")
 
+    V    = np.mean([np.load(f"../data/prob4/v_{i}.npy") for i in range(1,9)], axis = 0)
+    size = np.mean([np.load(f"../data/prob4/vs_{i}.npy") for i in range(1,9)], axis = 0)
+    
+    fig2, ax = plt.subplots()
+
+    plt.plot(V,size,label = r"$\mathcal{S}(v)$", ls = "--", marker = "o", color ="blue")
+    plt.xlabel(r"velocity $v$")
+    plt.ylabel(r"Size of crater $\mathcal{S}$")
+
+    annotation.slope_marker((5,500), (2,1), invert=True, 
+                ax=ax,text_kwargs={"fontsize" : 22})
+
+    plt.legend()
+    plt.yscale("log")
+    plt.xscale("log")
+    plt.grid(ls = "--")
+    plt.tight_layout()
+
+    fig2.savefig("../fig/vel_size.pdf")
 
 import sys
 
 if __name__ == "__main__":
 
     label = int(sys.argv[1])
-    mass_scan(10,label,1,25)
+    #mass_scan(10,label,1,25)
+    vel_scan(10,label)
 
     
 
